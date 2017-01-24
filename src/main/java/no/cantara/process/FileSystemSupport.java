37b7d6c193f053bd878184934142295d05808af2
totto <totto@totto.org>
2017-01-24 06:06:07 +0100

    initial work on process exec fallback producer

---
+++ src/main/java/no/cantara/process/FileSystemSupport.java
@@ -0,0 +1,55 @@
+package no.cantara.process;
+
+import java.nio.file.FileSystems;
+
+/**
+ * Created by ora on 10/20/16.
+ */
+public class FileSystemSupport {
+
+    public static String getOSString() {
+        return System.getProperties().get("os.name") + " " + System.getProperties().get("os.version");
+    }
+
+    public static boolean isLinux() {
+        return (getOSString().contains("Linux"));
+    }
+
+    public static boolean isLinuxFileSystem() {
+        return (isLinux() && "LinuxFileSystem".equals(FileSystems.getDefault().getClass().getSimpleName()));
+    }
+
+    public static boolean isMacOS() {
+        return ((getOSString().contains("MacOS")) || (getOSString().contains("OS X")));
+    }
+
+    public static boolean isMacOSFileSystem() {
+        return (isMacOS() && "MacOSXFileSystem".equals(FileSystems.getDefault().getClass().getSimpleName()));
+    }
+
+    public static boolean hasJDKEventDrivenFileSystem() {
+        return ((isLinuxFileSystem() || isWindowsFileSystem()) && !isMacOSFileSystem());
+    }
+
+    public static boolean isWindows() {
+        return getOSString().contains("Windows");
+    }
+
+    public static boolean isWindowsFileSystem() {
+        return (isWindows() && "WindowsFileSystem".equals(FileSystems.getDefault().getClass().getSimpleName()));
+    }
+
+    public static String execCmd(String cmd) throws java.io.IOException {
+        Process proc = Runtime.getRuntime().exec(cmd);
+        java.io.InputStream is = proc.getInputStream();
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        String val = "";
+        if (s.hasNext()) {
+            val = s.next();
+        } else {
+            val = "";
+        }
+        return val;
+    }
+
+}
